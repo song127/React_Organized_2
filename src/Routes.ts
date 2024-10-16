68f5ba205250b790a338c45f14ea8383b4af1a30
song127 <thdtmdwo789@naver.com>
2024-10-16 14:42:09 +0900

    chore: comment unuse routes

--- src/Routes.ts
+++ src/Routes.ts
@@ -2,17 +2,17 @@ import { createRoutes } from "@/utils/customObjects/CustomRoute";
 
 export const ROUTES = createRoutes({
   MAIN: "/",
-  TEST: "/test",
-  BUTTON: "/button",
-  INPUT: "/input",
   MODAL: "/modal",
-  SNACKBAR: "/snackbar",
-  SPECIAL: "/special",
   VALIDATION: "/validation",
-  PROFILE: {
-    BASE: "/profile",
-    LOGIN: "/login",
-    REGISTER: "/register",
-  },
+  SNACKBAR: "/snackbar",
+  // INPUT: "/input",
+  // BUTTON: "/button",
+  // SPECIAL: "/special",
+  // PROFILE: {
+  //   BASE: "/profile",
+  //   LOGIN: "/login",
+  //   REGISTER: "/register",
+  // },
+  // TEST: "/test",
   NOT_FOUND: "*",
 });
